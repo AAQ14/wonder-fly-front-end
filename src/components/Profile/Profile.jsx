787e1498975af08d4ce3a0be3c5ae801e1088a3d
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-18 15:04:12 +0300

    getting details based on the user logged in

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { userDetails } from "../../services/userService";
 
-const Profile = () => {
+const Profile = ({ userId }) => {
+  const [user, setUser] = useState();
+
+  const getUserDetails = async () => {
+    const details = await userDetails(userId);
+    setUser(details);
+  };
+  useEffect(() => {
+    getUserDetails();
+  }, []);
+
+  console.log("this is user", user.firstName);
+
   return (
     <>
-    <h2>My profile</h2>
+      <h1>My profile</h1>
+      {user.map((one) => (
+        <>
+          <p>First name: {one.firstName}</p>
+          <p>Last name: {one.lastName}</p>
+          <p>Email: {one.email}</p>
+        </>
+      ))}
+      {/* <h4>Email: {user.email}</h4> */}
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,11 @@
 import axios from "axios"
-baseURL= import.meta.env.VITE_BACKEND_URL
+const baseURL= import.meta.env.VITE_BACKEND_URL
 
 const userDetails = async (id)=>{
     try {
         const url = `${baseURL}/users/${id}`
         const res = await axios.get(url)
+        console.log(url)
         return res.data
     } catch (err) {
         console.log(err)
