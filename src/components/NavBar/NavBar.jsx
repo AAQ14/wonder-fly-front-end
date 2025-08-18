8c08a7499bd9794867898f33d2ab44d4dc607c94
AMAL <hopealasfoorgmail.com>
2025-08-18 09:39:09 +0300

    updated navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 const NavBar = () => {
   return (
     <>
-    <Link to="/">HOME</Link>
+    <Link to="/">Home</Link>
     <Link to="/flights">Flights</Link>
     <Link to="/login">Login</Link>
     <Link to="/signup">Signup</Link>
