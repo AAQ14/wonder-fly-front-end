6d4b805ca655f9648dca6110fed4665d22df945b
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-18 08:55:05 +0300

    splitting handleAddFlight function from handleSubmit

--- src/components/Flights/FlightForm/FlightForm.jsx
+++ src/components/Flights/FlightForm/FlightForm.jsx
@@ -2,43 +2,47 @@ import React from "react";
 import { useState } from "react";
 import { create } from "../../../services/flightService";
 
-const FlightForm = ({getAllFlights, handleFormView, selected, setFormIsShown}) => {
-const [isSubmitting, setIsSubmitting] = useState(false)
-const initialState = {
+const FlightForm = ({
+  getAllFlights,
+  handleFormView,
+  selected,
+  setFormIsShown,
+}) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const initialState = {
     from: "",
     to: "",
     Date: "",
     price: "",
-  }
-  const [formData, setFormData] = useState(
-    selected ? selected : initialState
-  );
+  };
+  const [formData, setFormData] = useState(selected ? selected : initialState);
 
-  const handleChange = (evt)=>{
-    setFormData({...formData, [evt.target.name]: evt.target.value})
-    console.log(formData)
-  }
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+    console.log(formData);
+  };
 
-  async function handleSubmit(evt) {
-    evt.preventDefault()
-    if(isSubmitting)return
-    setIsSubmitting(true)
-    const res = await create(formData)
-    
-    if(res.status=== 500){
-      console.log("invalid data")
+  async function handleSubmit(evt) {}
+  async function handleAddFlight(evt) {
+    evt.preventDefault();
+    if (isSubmitting) return;
+    setIsSubmitting(true);
+    const res = await create(formData);
+
+    if (res.status === 500) {
+      console.log("invalid data");
     }
-    getAllFlights()
+    getAllFlights();
     setFormData({
-    from: "",
-    to: "",
-    Date: "",
-    price: "",
-  })
-  if(res.status == 201){
-      setFormIsShown(false)
+      from: "",
+      to: "",
+      Date: "",
+      price: "",
+    });
+    if (res.status == 201) {
+      setFormIsShown(false);
     }
-    setIsSubmitting(false)
+    setIsSubmitting(false);
   }
 
   return (
@@ -47,24 +51,24 @@ const initialState = {
       <form onSubmit={handleSubmit}>
         <label htmlFor="from">From: </label>
         <select name="from" value={formData.from} onChange={handleChange}>
-          <option value="68a1479317f3896ce2244efd" >Bahrain</option>
-          <option value="68a17e3a0b393e504857bf44" >China</option>
-          <option value="68a1930c64bc454b60482035" >Jordan</option>
-          <option value="68a180050b393e504857bf46" >Italy</option>
-          <option value="68a1932164bc454b60482037" >Mexico</option>
-          <option value="68a1932664bc454b60482039"  >Peru</option>
-          <option value="68a1933164bc454b6048203b"  >Rio de Janeiro</option>
+          <option value="68a1479317f3896ce2244efd">Bahrain</option>
+          <option value="68a17e3a0b393e504857bf44">China</option>
+          <option value="68a1930c64bc454b60482035">Jordan</option>
+          <option value="68a180050b393e504857bf46">Italy</option>
+          <option value="68a1932164bc454b60482037">Mexico</option>
+          <option value="68a1932664bc454b60482039">Peru</option>
+          <option value="68a1933164bc454b6048203b">Rio de Janeiro</option>
         </select>
 
         <label htmlFor="to">to: </label>
         <select name="to" value={formData.to} onChange={handleChange}>
-          <option value="68a1479317f3896ce2244efd" >Bahrain</option>
-          <option value="68a17e3a0b393e504857bf44" >China</option>
-          <option value="68a1930c64bc454b60482035" >Jordan</option>
-          <option value="68a180050b393e504857bf46" >Italy</option>
-          <option value="68a1932164bc454b60482037" >Mexico</option>
-          <option value="68a1932664bc454b60482039"  >Peru</option>
-          <option value="68a1933164bc454b6048203b"  >Rio de Janeiro</option>
+          <option value="68a1479317f3896ce2244efd">Bahrain</option>
+          <option value="68a17e3a0b393e504857bf44">China</option>
+          <option value="68a1930c64bc454b60482035">Jordan</option>
+          <option value="68a180050b393e504857bf46">Italy</option>
+          <option value="68a1932164bc454b60482037">Mexico</option>
+          <option value="68a1932664bc454b60482039">Peru</option>
+          <option value="68a1933164bc454b6048203b">Rio de Janeiro</option>
         </select>
 
         <label htmlFor="Date">Date: </label>
