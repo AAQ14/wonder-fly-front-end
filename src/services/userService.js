868136cbf14e272ab1f80ffc66383259c15db7c1
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-18 14:44:33 +0300

    userDetails function updated

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,7 +4,8 @@ baseURL= import.meta.env.VITE_BACKEND_URL
 const userDetails = async (id)=>{
     try {
         const url = `${baseURL}/users/${id}`
-
+        const res = await axios.get(url)
+        return res.data
     } catch (err) {
         console.log(err)
     }
