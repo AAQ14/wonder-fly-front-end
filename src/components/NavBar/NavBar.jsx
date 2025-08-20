8c2b374c043458a6a2742f5626dfa570b9337541
AMAL <hopealasfoorgmail.com>
2025-08-20 15:06:26 +0300

    evrything abt navbar is done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import LogoutButton from "../LogoutButton/LogoutButton";
 
 const NavBar = (props) => {
   return (
@@ -7,15 +8,19 @@ const NavBar = (props) => {
       {props.token ? (
         <>
         <div className="Nav">
+          <div className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/flights">Flights</Link>
           <Link to="/profile">My profile</Link>
+          </div>
+          <div className="logoutbtn">
+            {props.token ? <LogoutButton onLogout={props.handleLogout} /> : null}
+          </div>
        </div>
         </>
       ) : (
         <>
         <div className="Nav2">
-          <Link to="/">Home</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
           </div>
