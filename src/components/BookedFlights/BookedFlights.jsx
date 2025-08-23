59742ee026f559500458b862ac831acb8d337e9b
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-23 15:32:12 +0300

    fadeLoader added to components

--- src/components/BookedFlights/BookedFlights.jsx
+++ src/components/BookedFlights/BookedFlights.jsx
@@ -1,47 +1,58 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import {userDetails, cancelFlight} from '../../services/userService'
+import React from "react";
+import { useEffect, useState } from "react";
+import { userDetails, cancelFlight } from "../../services/userService";
 import { FadeLoader } from "react-spinners";
 
+const BookedFlights = ({ userId }) => {
+  const [user, setUser] = useState({});
 
-const BookedFlights = ({userId}) => {
-    const [user,setUser] = useState({})
-
-    const getUser = async()=>{
-        try {
-            const userFound = await userDetails(userId) 
-            setUser(userFound)
-        } catch (err) {
-            console.log(err)
-        }
+  const getUser = async () => {
+    try {
+      const userFound = await userDetails(userId);
+      setUser(userFound);
+    } catch (err) {
+      console.log(err);
     }
-    useEffect(()=>{getUser()},[userId])
-    console.log(user)
+  };
+  useEffect(() => {
+    getUser();
+  }, [userId]);
+  console.log(user);
 
-    const handleCancel = async(flight) =>{
-      try {
-        const updated = user.bookedFlights.filter(flightt => flight !== flightt)
-         setUser({...user, bookedFlights: updated})
-         alert("flight cancelled successfully")
-         await cancelFlight(userId, flight)
-         
-      } catch (err) {
-        console.log(err)
-      }
+  const handleCancel = async (flight) => {
+    try {
+      const updated = user.bookedFlights.filter(
+        (flightt) => flight !== flightt
+      );
+      setUser({ ...user, bookedFlights: updated });
+      alert("flight cancelled successfully");
+      await cancelFlight(userId, flight);
+    } catch (err) {
+      console.log(err);
     }
+  };
   return (
-    <> 
-    {user? user.bookedFlights?.map(flight => (
-      <div  className='trip'>
-        <p>flight number: {flight._id}</p>
-        <p>from: {flight.from.country}</p>
-        <p>to: {flight.to.country}</p>
-        <p>date: {flight.date}</p>
-         <div className='BookAndCancelBtns' onClick={()=>handleCancel(flight)}>Cancel flight</div>
-      </div>
-)): <FadeLoader />}
+    <>
+      { user?.bookedFlights?.length === 0 ? (
+        <p>no flights booked</p>
+      ) : Object.keys(user).length  ? (
+        user.bookedFlights?.map((flight) => (
+          <div className="trip">
+            <p>flight number: {flight._id}</p>
+            <p>from: {flight.from.country}</p>
+            <p>to: {flight.to.country}</p>
+            <p>date: {flight.date}</p>
+            <div
+              className="BookAndCancelBtns"
+              onClick={() => handleCancel(flight)}
+            >
+              Cancel flight
+            </div>
+          </div>
+        ))
+      ) : <FadeLoader />}
     </>
-  )
-}
+  );
+};
 
-export default BookedFlights
+export default BookedFlights;
